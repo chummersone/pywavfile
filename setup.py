7fa691636925bcee33d0c188e497662740860c91
Chris Hummersone <17592217+chummersone@users.noreply.github.com>
2022-08-19 13:01:19 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -25,7 +25,11 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages('src'),
     package_dir={'': 'src'},
